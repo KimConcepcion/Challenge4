28707eb1af5241e1d564bd2a9afa426e13a0a41b
Kim Nielsen <kimnielsen33333@gmail.com>
2018-11-14 23:37:11 +0100

    Last touch for now...

--- app/src/main/java/dk/pme/kim/challenge4/Repository.kt
+++ app/src/main/java/dk/pme/kim/challenge4/Repository.kt
@@ -4,5 +4,6 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 data class Owner(val login : String)
-data class Repo (var name : String, var owner : Owner, var html_url : String, var updated_at : String, var stargazers_count : Int)
+data class Repo (var name : String, var owner : Owner, var html_url : String,
+                 var updated_at : String, var stargazers_count : Int)
 data class Repos(var reposList : ArrayList<Repo>)
